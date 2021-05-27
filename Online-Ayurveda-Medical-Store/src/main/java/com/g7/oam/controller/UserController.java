b566a96c66a4b92919b60336552ac4ac2edca017
utkarshb29 <utkarshb29@gmail.com>
2021-05-27 09:21:32 +0530

    usercontroller done

--- Online-Ayurveda-Medical-Store/src/main/java/com/g7/oam/controller/UserController.java
+++ Online-Ayurveda-Medical-Store/src/main/java/com/g7/oam/controller/UserController.java
@@ -1,14 +1,25 @@
 package com.g7.oam.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.g7.oam.entities.Customer;
+import com.g7.oam.entities.User;
 import com.g7.oam.service.IUserService;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
 	@Autowired
 	IUserService userService;
 
+	@GetMapping("/showAll")
+	public List<User> showAllUsers() {
+		return this.userService.showAllUsers();
+	}
 }

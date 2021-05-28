424048f131fce79b7fd17cc13a16ccb6e50ce68b
utkarshb29 <utkarshb29@gmail.com>
2021-05-28 10:03:05 +0530

    Global Exceptions added

--- Online-Ayurveda-Medical-Store/src/main/java/com/g7/oam/exception/GlobalExceptionHandler.java
+++ Online-Ayurveda-Medical-Store/src/main/java/com/g7/oam/exception/GlobalExceptionHandler.java
@@ -8,26 +8,39 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	
+
 	@ExceptionHandler(CustomerNotFoundException.class)
 	public ResponseEntity<String> handleCustomerException(CustomerNotFoundException e) {
 		HttpHeaders header = new HttpHeaders();
 		header.add("Description", "Trying to get a Customer...");
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(header).body(e.getMessage();
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(header).body(e.getMessage());
 	}
-	
+
 	@ExceptionHandler(CategoryNotFoundException.class)
 	public ResponseEntity<String> handleCategoryException(CategoryNotFoundException e) {
 		HttpHeaders header = new HttpHeaders();
 		header.add("Description", "Trying to get a Category...");
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(header).body(e.getMessage());
 	}
-	
+
 	@ExceptionHandler(OrderNotFoundException.class)
 	public ResponseEntity<String> handleOrderException(OrderNotFoundException e) {
 		HttpHeaders header = new HttpHeaders();
-		header.add("Description", "Trying to get a Category...");
+		header.add("Description", "Trying to get a Order...");
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(header).body(e.getMessage());
 	}
 
+	@ExceptionHandler(AdminNotFoundException.class)
+	public ResponseEntity<String> handleAdminException(AdminNotFoundException e) {
+		HttpHeaders header = new HttpHeaders();
+		header.add("Description", "Trying to get a Admin...");
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(header).body(e.getMessage());
+	}
+
+	@ExceptionHandler(MedicineNotFoundException.class)
+	public ResponseEntity<String> handleMedicineException(MedicineNotFoundException e) {
+		HttpHeaders header = new HttpHeaders();
+		header.add("Description", "Trying to get a Medicine...");
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(header).body(e.getMessage());
+	}
 }
